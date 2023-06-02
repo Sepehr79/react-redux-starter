369dd67f8fd6c3c96a0a08a647b6fd54884e26e0
Sepehr79 <sepehrmsm1379@gmail.com>
2023-06-02 22:44:58 +0330

    testing react components

---
+++ src/todos/tests/selectors.test.js
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import { getCompletedTodos } from "../selectors";
+
+describe('The getCompletedTodosSelector', () => {
+    it('Returns only complted todos', () => {
+        
+    })
+})
